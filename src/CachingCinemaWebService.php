0248542a514a40a227392a7d269b2bea21d21108
Micha Alt <mail@lean-stack.de>
2015-11-17 14:57:07 +0100

    First cached method: eventsForCinema

--- src/CachingCinemaWebService.php
+++ src/CachingCinemaWebService.php
@@ -8,6 +8,23 @@
 
 namespace LeanStack\CinetixxClient;
 
+use Lean\Cache\CacheItemPool;
+
+use LeanStack\CinetixxClient\Auth\Token;
+
+use LeanStack\CinetixxClient\DTO\Event;
+use LeanStack\CinetixxClient\DTO\EventInformation;
+use LeanStack\CinetixxClient\DTO\Image;
+use LeanStack\CinetixxClient\DTO\ShowDetail;
+use LeanStack\CinetixxClient\DTO\TypedText;
+
+use LeanStack\CinetixxClient\Message\GetEventImagesRequest;
+use LeanStack\CinetixxClient\Message\GetEventInformationRequest;
+use LeanStack\CinetixxClient\Message\GetEventInformationResponse;
+use LeanStack\CinetixxClient\Message\GetEventsForCinemaRequest;
+use LeanStack\CinetixxClient\Message\GetMovieInformationRequest;
+use LeanStack\CinetixxClient\Message\GetShowsForEventRequest;
+
 /**
  * Description of CachingCinemaWebService
  *
@@ -20,12 +37,28 @@ class CachingCinemaWebService extends CinemaWebService {
     
     public function __construct(Token $authToken, $debug = false) {
 
-            parent::__construct($authToken, $debug);
+        parent::__construct($authToken, $debug);
 
-            $options = \phpFastCache::$config;
-            $options['path'] = '/www/htdocs/w005eb31/tmp';
-            $options['securityKey'] = 'ctx';
-            
-            $this->cache = new CacheItemPool('sqlite', $options);
-	}
+        $options = \phpFastCache::$config;
+        $options['path'] = '/www/htdocs/w005eb31/tmp';
+        $options['securityKey'] = 'ctx';
+
+        $this->cache = new CacheItemPool('sqlite', $options);
+    }
+    
+    public function GetEventsForCinema($cinemaId, \DateTime $dateFrom, \DateTime $dateUntil) {
+        
+        $key = 'events';
+        $events = null;
+        
+        $item = $this->cache->getItem($key);
+        if( $item->isHit() ) {
+            $events = $item->get();
+        } else {
+            $events = parent::GetEventsForCinema($cinemaId, $dateFrom, $dateUntil);
+            $item->set($events);
+        }
+        
+        return $events;
+    }
 }
